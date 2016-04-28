6683da4f16e26387af9e661b13209d2eaaec9b6a
Safiyat Reza <md.reza@snapdeal.com>
2016-04-28 15:29:57 +0530

    Functionalized all. Will add parameters once OS variable reader is up.

--- keystoneutils.py
+++ keystoneutils.py
@@ -1,9 +1,23 @@
 from keystoneclient.auth.identity import v3
-from keystoneclient import session
+from keystoneclient import session as ksession
 from keystoneclient.v3 import client
 
-auth = v3.Password(auth_url='http://controller:35357/v3', password='snapdeal@1234', username='osadmin', user_domain_id='default', project_name='admin', project_domain_id='default')
 
-sess = session.Session(auth=auth)
+def get_auth():
+    auth = v3.Password(auth_url='http://controller:35357/v3',
+                       password='snapdeal@1234', username='osadmin',
+                       user_domain_id='default', project_name='admin',
+                       project_domain_id='default')
+    return auth
 
-keystone = client.Client(session=sess)
+
+def get_session(auth):
+    session = ksession.Session(auth=auth)
+    return session
+
+
+def get_client():
+    auth = get_auth()
+    session = get_session(auth)
+    keystone = client.Client(session=session)
+    return keystone
